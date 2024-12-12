6b22ecd18ecf8e4444b2a35aa52c217a7a9c0582
khushbujain41709 <jkhushbu970@gmail.com>
2024-12-12 21:45:32 +0530

    Added files for ContactUs Page

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+let dotenv = require("dotenv").config();
+
+const app = express();
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const postRoutes = require('./routes/postRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Route handlers
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+// app.use('/api/search', searchRoutes);
+
+// Serve index.html on root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: "myapp"
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Export the Express app
+module.exports = app;
